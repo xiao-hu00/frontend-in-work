c71dfa7d9efda4023bb9b2ce670199e246f5dffc
xiao-hu00 <406659545@qq.com>
2024-08-16 11:22:35 +0800

    resizable

--- project-apps/next-app/app/page.tsx
+++ project-apps/next-app/app/page.tsx
@@ -1,12 +1,23 @@
 'use client'
 
-import Image from 'next/image'
+// import Image from 'next/image'
+import { PanelGroup, Panel, PanelResizeHandle, disableGlobalCursorStyles } from 'react-resizable-panels'
 
 export default function Home() {
+  // disableGlobalCursorStyles()
+  const onChange = (e: any) => {
+    console.log(e)
+  }
   return (
     <div className='flex justify-center'>
       <div className='max-w-[1200px]'>
-        <Image src='/05.jpg' alt='main' width={1200} height={615} />
+        resizable
+        {/* <Image src='/05.jpg' alt='main' width={1200} height={615} /> */}
+        <PanelGroup direction='vertical' style={{ height: 500, width: 800, border: '1px solid #ddd' }}>
+          <Panel maxSize={75}>top</Panel>
+          <PanelResizeHandle style={{ height: 2, background: '#333' }} />
+          <Panel maxSize={75}>bottom</Panel>
+        </PanelGroup>
       </div>
     </div>
   )
